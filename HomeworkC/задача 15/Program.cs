23b8912353c9038474122250607a3fa74a40cd6a
Kirill Ushakov <faberest@list.ru>
2023-03-12 21:34:08 +1000

    15 zadacha

---
+++ HomeworkC/задача 15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число от 1 до 7 ");
+int n = int.Parse(Console.ReadLine()!);
+while (n > 7 || n < 1)
+{
+  Console.WriteLine("Введите другую цифру ");
+  n = int.Parse(Console.ReadLine()!);
+}
+if (n == 6 || n == 7)
+{
+  Console.WriteLine("Выходной ");
+}
+else
+{
+  Console.WriteLine("Будний ");
+}
