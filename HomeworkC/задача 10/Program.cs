b5dedea12154f27d5613e41ea1597330a46fbff8
Kirill Ushakov <faberest@list.ru>
2023-03-12 22:17:15 +1000

    ispravil

--- HomeworkC/задача 10/Program.cs
+++ HomeworkC/задача 10/Program.cs
@@ -1,5 +1,10 @@
 ﻿Console.Clear();
 Console.Write("Введите трехзначное число ");
 int n = int.Parse(Console.ReadLine()!);
+  while (n<100 || n> 999)
+  {
+  Console.WriteLine("введите трехзначное число ");
+  n = int.Parse(Console.ReadLine()!);
+  }
 int n1 = n / 10 % 10;
 Console.WriteLine($"{n1}");
