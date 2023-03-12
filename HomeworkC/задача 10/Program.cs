4742fd26244f6f051da8ffe4dac80b2dd31f4782
Kirill Ushakov <faberest@list.ru>
2023-03-12 20:22:10 +1000

    13 zadacha

---
+++ HomeworkC/задача 10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число ");
+int n = int.Parse(Console.ReadLine()!);
+int n1 = n / 10 % 10;
+Console.WriteLine($"{n1}");
---
+++ HomeworkC/задача 13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("введите число ");
+int n = int.Parse(Console.ReadLine()!);
+if (n < 100)
+  Console.WriteLine("третьей цифры нет ");
+else 
+{
+  if(n>999)
+  {
+    while (n>999)
+    n=n/10;
+  }
+Console.WriteLine($"третья цифра {n%10} ");
+}
+
