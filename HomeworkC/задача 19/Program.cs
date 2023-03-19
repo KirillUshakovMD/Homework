7849aab93f7ddbb503708c8a2a4f51d320d9381f
Kirill Ushakov <faberest@list.ru>
2023-03-19 16:58:00 +1000

    zadacha 19

---
+++ HomeworkC/задача 19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("введите пятизначное число ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 9999 || n > 100000)
+{
+  Console.WriteLine("ошибка, введите пятизначное число ");
+  n = int.Parse(Console.ReadLine()!);
+}
+string n1 = n.ToString();
+if (n1[0] == n1[4] && n1[1] == n1[3])
+  Console.WriteLine("palindrom");
+else
+{
+  Console.WriteLine("ne palindrom");
+}
