89326217dd3d2efa2ae9d91f2871481624e7c54a
Kirill Ushakov <faberest@list.ru>
2023-03-26 15:04:47 +1000

    zadacha 36

---
+++ HomeworkC/задача 36/Program.cs
@@ -0,0 +1,22 @@
+﻿void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-100, 100);
+}
+int SumNechet(int[] array)
+{
+  int result = 0;
+  for (int i = 1; i < array.Length; i=i+2)
+  {
+    result += array[i];
+  }
+return result;
+}
+Console.Clear();
+Console.WriteLine("vvedite chislo elementov massiva ");
+int n = int.Parse(Console.ReadLine()!);
+int[]array=new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ",array)}]");
+SumNechet(array);
+Console.WriteLine(SumNechet(array));
