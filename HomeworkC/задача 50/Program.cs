4ec5da636ce0dc914ead3a6094ea3c889e7bb94c
Kirill Ushakov <faberest@list.ru>
2023-04-03 19:37:52 +1000

    52 zadacha

---
+++ HomeworkC/задача 50/Program.cs
@@ -0,0 +1,35 @@
+﻿void InputMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      matrix[i, j] = new Random().Next(0, 50);
+
+  }
+}
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      Console.Write($"{matrix[i, j]}\t");
+    Console.WriteLine();
+  }
+}
+Console.Write("введите размерность массива ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine("введите номер строки ");
+int n =int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите номер столбца ");
+int m =int.Parse(Console.ReadLine()!);
+if(n>matrix.GetLength(0)||m>matrix.GetLength(1))
+{
+  Console.WriteLine("такой позиции нет ");
+}
+else
+{
+  Console.WriteLine($"{matrix[n-1,m-1]}");
+}
---
+++ HomeworkC/задача 52/Program.cs
@@ -0,0 +1,38 @@
+﻿void InputMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      matrix[i, j] = new Random().Next(0, 11);
+
+  }
+}
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      Console.Write($"{matrix[i, j]}\t");
+    Console.WriteLine();
+  }
+}
+void SumStolb(int[,] matrix)
+{
+  for (int j = 0; j < matrix.GetLength(1); j++)
+  {
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      sum = sum + matrix[i, j];
+    }
+  Console.WriteLine($"{sum / matrix.GetLength(0)}");
+  }
+}
+Console.Clear();
+Console.Write("Введите размерность массива ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+SumStolb(matrix);
