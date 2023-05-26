62088a6d8a97afa5264dbc27c77c8513bbe3bb7e
Kirill Ushakov <faberest@list.ru>
2023-05-26 22:10:06 +1000

    add итоговая работа

---
+++ HomeworkC/задача 66/Program.cs
@@ -0,0 +1,14 @@
+﻿string f(int a, int b)
+{
+  if (a == 0)
+    return $"{b} ";
+  return f(a - 1, b+ 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
