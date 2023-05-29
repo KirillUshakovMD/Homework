4906ceef8401c8d4a90312fbfa37e53233f6061d
Kirill Ushakov <faberest@list.ru>
2023-05-29 22:00:14 +1000

    the end

--- итоговая работа/Program.cs
+++ итоговая работа/Program.cs
@@ -15,11 +15,14 @@ for (int i = 0; i < n; i++)
   if (array[i].Length < 4)
     count++;
 }
-n=count;
-string[] newArray = new string[n];
+string[] newArray = new string[count];
+count = 0;
 for (int i = 0; i < n; i++)
 {
   if (array[i].Length < 4)
-    newArray[i] = array[i];
+  {
+    newArray[count] = array[i];
+    count++;
+  }
 }
 Console.WriteLine($"конечный массив: [{string.Join(", ", newArray)}]");
