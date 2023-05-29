8ca2d0427eb3ea4b8c8d196ec4c2e7f8ed5ccb75
Kirill Ushakov <faberest@list.ru>
2023-05-29 17:57:11 +1000

    finish01

--- итоговая работа/Program.cs
+++ итоговая работа/Program.cs
@@ -7,20 +7,19 @@ for (int i = 0; i < n; i++)
   Console.WriteLine("Введите строку: ");
   array[i] = Console.ReadLine()!;
 }
-
 Console.WriteLine($"начальный массив: [{string.Join(", ", array)}]");
-int count=0;
+
+int count = 0;
 for (int i = 0; i < n; i++)
 {
-if (array[i].Length<4)
-count++;
+  if (array[i].Length < 4)
+    count++;
 }
 n=count;
-string [] newArray=new string [n];
+string[] newArray = new string[n];
 for (int i = 0; i < n; i++)
 {
-int m = array[i].Length;
-if (m<4)
-newArray[i]=array[i];
+  if (array[i].Length < 4)
+    newArray[i] = array[i];
 }
 Console.WriteLine($"конечный массив: [{string.Join(", ", newArray)}]");
